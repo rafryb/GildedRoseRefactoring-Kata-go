a32db12f674c5d073a352ad09be6ab417e5a3e47
Rafal Rybicki <rafal.rybicki@fairmoney.io>
2024-05-27 14:30:30 +0200

    Move naming of items to constants

--- gildedrose/gildedrose.go
+++ gildedrose/gildedrose.go
@@ -5,21 +5,25 @@ type Item struct {
 	SellIn, Quality int
 }
 
-const ()
+const (
+	agedBrie      = "Aged Brie"
+	backstagePass = "Backstage passes to a TAFKAL80ETC concert"
+	sulfuras      = "Sulfuras, Hand of Ragnaros"
+)
 
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
 
-		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
+		if items[i].Name != agedBrie && items[i].Name != backstagePass {
 			if items[i].Quality > 0 {
-				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
+				if items[i].Name != sulfuras {
 					items[i].Quality = items[i].Quality - 1
 				}
 			}
 		} else {
 			if items[i].Quality < 50 {
 				items[i].Quality = items[i].Quality + 1
-				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
+				if items[i].Name == backstagePass {
 					if items[i].SellIn < 11 {
 						if items[i].Quality < 50 {
 							items[i].Quality = items[i].Quality + 1
@@ -34,15 +38,15 @@ func UpdateQuality(items []*Item) {
 			}
 		}
 
-		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
+		if items[i].Name != sulfuras {
 			items[i].SellIn = items[i].SellIn - 1
 		}
 
 		if items[i].SellIn < 0 {
-			if items[i].Name != "Aged Brie" {
-				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
+			if items[i].Name != agedBrie {
+				if items[i].Name != backstagePass {
 					if items[i].Quality > 0 {
-						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
+						if items[i].Name != sulfuras {
 							items[i].Quality = items[i].Quality - 1
 						}
 					}
