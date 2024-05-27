9b35833e0da2a005dca3c6662d62c7bbd835e150
Rafal Rybicki <rafal.rybicki@fairmoney.io>
2024-05-27 15:04:07 +0200

    Add test

--- gildedrose/gildedrose_test.go
+++ gildedrose/gildedrose_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/emilybache/gildedrose-refactoring-kata/gildedrose"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_Foo(t *testing.T) {
@@ -18,26 +19,93 @@ func Test_Foo(t *testing.T) {
 	}
 }
 
-//-------- day 0 --------
-//Name, SellIn, Quality
-//&{+5 Dexterity Vest 10 20}
-//&{Aged Brie 2 0}
-//&{Elixir of the Mongoose 5 7}
-//&{Sulfuras, Hand of Ragnaros 0 80}
-//&{Sulfuras, Hand of Ragnaros -1 80}
-//&{Backstage passes to a TAFKAL80ETC concert 15 20}
-//&{Backstage passes to a TAFKAL80ETC concert 10 49}
-//&{Backstage passes to a TAFKAL80ETC concert 5 49}
-//&{Conjured Mana Cake 3 6}
-//
-//-------- day 1 --------
-//Name, SellIn, Quality
-//&{+5 Dexterity Vest 9 19}
-//&{Aged Brie 1 1}
-//&{Elixir of the Mongoose 4 6}
-//&{Sulfuras, Hand of Ragnaros 0 80}
-//&{Sulfuras, Hand of Ragnaros -1 80}
-//&{Backstage passes to a TAFKAL80ETC concert 14 21}
-//&{Backstage passes to a TAFKAL80ETC concert 9 50}
-//&{Backstage passes to a TAFKAL80ETC concert 4 50}
-//&{Conjured Mana Cake 2 5}
+func Test_First_Day(t *testing.T) {
+	var items = []*gildedrose.Item{
+		{"+5 Dexterity Vest", 10, 20},
+		{"Aged Brie", 2, 0},
+		{"Elixir of the Mongoose", 5, 7},
+		{"Sulfuras, Hand of Ragnaros", 0, 80},
+		{"Sulfuras, Hand of Ragnaros", -1, 80},
+		{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+		{"Backstage passes to a TAFKAL80ETC concert", 10, 49},
+		{"Backstage passes to a TAFKAL80ETC concert", 5, 49},
+		{"Conjured Mana Cake", 3, 6}, // <-- :O
+	}
+
+	gildedrose.UpdateQuality(items)
+
+	assert.Equal(t, items[0].SellIn, 9)
+	assert.Equal(t, items[0].Quality, 19)
+
+	assert.Equal(t, items[1].SellIn, 1)
+	assert.Equal(t, items[1].Quality, 1)
+
+	assert.Equal(t, items[2].SellIn, 4)
+	assert.Equal(t, items[2].Quality, 6)
+
+	assert.Equal(t, items[3].SellIn, 0)
+	assert.Equal(t, items[3].Quality, 80)
+
+	assert.Equal(t, items[4].SellIn, -1)
+	assert.Equal(t, items[4].Quality, 80)
+
+	assert.Equal(t, items[5].SellIn, 14)
+	assert.Equal(t, items[5].Quality, 21)
+
+	assert.Equal(t, items[6].SellIn, 9)
+	assert.Equal(t, items[6].Quality, 50)
+
+	assert.Equal(t, items[7].SellIn, 4)
+	assert.Equal(t, items[7].Quality, 50)
+
+	assert.Equal(t, items[8].SellIn, 2)
+	assert.Equal(t, items[8].Quality, 5)
+}
+
+func Test_Thirty_Days(t *testing.T) {
+
+	var items = []*gildedrose.Item{
+		{"+5 Dexterity Vest", 10, 20},
+		{"Aged Brie", 2, 0},
+		{"Elixir of the Mongoose", 5, 7},
+		{"Sulfuras, Hand of Ragnaros", 0, 80},
+		{"Sulfuras, Hand of Ragnaros", -1, 80},
+		{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+		{"Backstage passes to a TAFKAL80ETC concert", 10, 49},
+		{"Backstage passes to a TAFKAL80ETC concert", 5, 49},
+		{"Conjured Mana Cake", 3, 6}, // <-- :O
+	}
+
+	days := 30
+	for day := 1; day < days; day++ {
+		gildedrose.UpdateQuality(items)
+	}
+
+	assert.Equal(t, items[0].SellIn, -19)
+	assert.Equal(t, items[0].Quality, 0)
+
+	assert.Equal(t, items[1].SellIn, -27)
+	assert.Equal(t, items[1].Quality, 50)
+
+	assert.Equal(t, items[2].SellIn, -24)
+	assert.Equal(t, items[2].Quality, 0)
+
+	assert.Equal(t, items[3].SellIn, 0)
+	assert.Equal(t, items[3].Quality, 80)
+
+	assert.Equal(t, items[4].SellIn, -1)
+	assert.Equal(t, items[4].Quality, 80)
+
+	assert.Equal(t, items[5].SellIn, -14)
+	assert.Equal(t, items[5].Quality, 0)
+
+	assert.Equal(t, items[6].SellIn, -19)
+	assert.Equal(t, items[6].Quality, 0)
+
+	assert.Equal(t, items[7].SellIn, -24)
+	assert.Equal(t, items[7].Quality, 0)
+
+	assert.Equal(t, items[8].SellIn, -26)
+	assert.Equal(t, items[8].Quality, 0)
+
+}
