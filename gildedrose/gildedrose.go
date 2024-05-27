c5de4dba29e73eb3894ed0e25c9226838d9e7bb0
Rafal Rybicki <rafal.rybicki@fairmoney.io>
2024-05-27 17:09:06 +0200

    Add regexp matching and conjured busiess logic

--- gildedrose/gildedrose.go
+++ gildedrose/gildedrose.go
@@ -1,5 +1,7 @@
 package gildedrose
 
+import "strings"
+
 type Item struct {
 	Name            string
 	SellIn, Quality int
@@ -7,74 +9,28 @@ type Item struct {
 
 const (
 	agedBrie      = "Aged Brie"
-	backstagePass = "Backstage passes to a TAFKAL80ETC concert"
-	sulfuras      = "Sulfuras, Hand of Ragnaros"
+	backstagePass = "Backstage passes"
+	sulfuras      = "Sulfuras"
+	conjured      = "Conjured"
 	minQuality    = 0
 	maxQuality    = 50
 )
 
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
-		switch items[i].Name {
-		case agedBrie:
+		switch {
+		case strings.Contains(items[i].Name, agedBrie):
 			updateAgedBrie(items[i])
-		case backstagePass:
+		case strings.Contains(items[i].Name, backstagePass):
 			updateBackstagePass(items[i])
-		case sulfuras:
+		case strings.Contains(items[i].Name, sulfuras):
 			continue
+		case strings.Contains(items[i].Name, conjured):
+			updateConjured(items[i])
 		default:
 			updateDefaultItem(items[i])
 		}
 	}
-	//for i := 0; i < len(items); i++ {
-
-	//	if items[i].Name != agedBrie && items[i].Name != backstagePass {
-	//		if items[i].Quality > 0 {
-	//			if items[i].Name != sulfuras {
-	//				items[i].Quality = items[i].Quality - 1
-	//			}
-	//		}
-	//	} else {
-	//		if items[i].Quality < 50 {
-	//			items[i].Quality = items[i].Quality + 1
-	//			if items[i].Name == backstagePass {
-	//				if items[i].SellIn < 11 {
-	//					if items[i].Quality < 50 {
-	//						items[i].Quality = items[i].Quality + 1
-	//					}
-	//				}
-	//				if items[i].SellIn < 6 {
-	//					if items[i].Quality < 50 {
-	//						items[i].Quality = items[i].Quality + 1
-	//					}
-	//				}
-	//			}
-	//		}
-	//	}
-	//
-	//	if items[i].Name != sulfuras {
-	//		items[i].SellIn = items[i].SellIn - 1
-	//	}
-	//
-	//	if items[i].SellIn < 0 {
-	//		if items[i].Name != agedBrie {
-	//			if items[i].Name != backstagePass {
-	//				if items[i].Quality > 0 {
-	//					if items[i].Name != sulfuras {
-	//						items[i].Quality = items[i].Quality - 1
-	//					}
-	//				}
-	//			} else {
-	//				items[i].Quality = items[i].Quality - items[i].Quality
-	//			}
-	//		} else {
-	//			if items[i].Quality < 50 {
-	//				items[i].Quality = items[i].Quality + 1
-	//			}
-	//		}
-	//	}
-	//}
-
 }
 
 func updateAgedBrie(item *Item) {
@@ -87,10 +43,10 @@ func agedBrieQuality(item *Item) int {
 		return item.Quality
 	}
 
-	if item.SellIn > minQuality {
-		return item.Quality + 1
+	if item.SellIn > 0 {
+		return sumQuality(item, 1)
 	} else {
-		return item.Quality + 2
+		return sumQuality(item, 2)
 	}
 }
 
@@ -105,19 +61,60 @@ func backstagePassQuality(item *Item) int {
 	}
 
 	if item.SellIn > 10 {
-		return item.Quality + 1
+		return sumQuality(item, 1)
 	}
 
 	if item.SellIn > 5 {
-		return item.Quality + 2
+		return sumQuality(item, 2)
 	}
 
 	if item.SellIn > 0 {
-		return item.Quality + 3
+		return sumQuality(item, 3)
 	}
 
 	return 0
 }
 
 func updateDefaultItem(item *Item) {
+	item.Quality = defaultItemQuality(item)
+	item.SellIn -= 1
+}
+
+func defaultItemQuality(item *Item) int {
+	if item.Quality == minQuality {
+		return item.Quality
+	}
+
+	if item.SellIn > 0 {
+		return sumQuality(item, -1)
+	} else {
+		return sumQuality(item, -2)
+	}
+}
+
+func updateConjured(item *Item) {
+	item.Quality = conjuredQuality(item)
+	item.SellIn -= 1
+}
+
+func conjuredQuality(item *Item) int {
+	if item.Quality == minQuality {
+		return item.Quality
+	}
+
+	if item.SellIn > 0 {
+		return sumQuality(item, -2)
+	} else {
+		return sumQuality(item, -4)
+	}
+}
+
+func sumQuality(item *Item, quality int) int {
+	sum := item.Quality + quality
+
+	if sum > maxQuality {
+		return maxQuality
+	}
+
+	return sum
 }
--- gildedrose/gildedrose_test.go
+++ gildedrose/gildedrose_test.go
@@ -59,7 +59,7 @@ func Test_First_Day(t *testing.T) {
 	assert.Equal(t, items[7].Quality, 50)
 
 	assert.Equal(t, items[8].SellIn, 2)
-	assert.Equal(t, items[8].Quality, 5)
+	assert.Equal(t, items[8].Quality, 4)
 }
 
 func Test_Aged_Brie_30_Days(t *testing.T) {
@@ -116,41 +116,16 @@ func Test_Sulfuras_30_Days(t *testing.T) {
 	assert.Equal(t, items[1].Quality, 80)
 }
 
-//var items = []*gildedrose.Item{
-//	{"+5 Dexterity Vest", 10, 20},
-//	{"Aged Brie", 2, 0},
-//	{"Elixir of the Mongoose", 5, 7},
-//	{"Sulfuras, Hand of Ragnaros", 0, 80},
-//	{"Sulfuras, Hand of Ragnaros", -1, 80},
-//	{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
-//	{"Backstage passes to a TAFKAL80ETC concert", 10, 49},
-//	{"Backstage passes to a TAFKAL80ETC concert", 5, 49},
-//	{"Conjured Mana Cake", 3, 6}, // <-- :O
-//}
+func Test_Conjured_30_Days(t *testing.T) {
+	var items = []*gildedrose.Item{
+		{"Conjured Mana Cake", 3, 6}, // <-- :O
+	}
 
-//assert.Equal(t, items[0].SellIn, -19)
-//assert.Equal(t, items[0].Quality, 0)
-//
-//assert.Equal(t, items[1].SellIn, -27)
-//assert.Equal(t, items[1].Quality, 50)
-//
-//assert.Equal(t, items[2].SellIn, -24)
-//assert.Equal(t, items[2].Quality, 0)
-//
-//assert.Equal(t, items[3].SellIn, 0)
-//assert.Equal(t, items[3].Quality, 80)
-//
-//assert.Equal(t, items[4].SellIn, -1)
-//assert.Equal(t, items[4].Quality, 80)
-//
-//assert.Equal(t, items[5].SellIn, -14)
-//assert.Equal(t, items[5].Quality, 0)
-//
-//assert.Equal(t, items[6].SellIn, -19)
-//assert.Equal(t, items[6].Quality, 0)
-//
-//assert.Equal(t, items[7].SellIn, -24)
-//assert.Equal(t, items[7].Quality, 0)
-//
-//assert.Equal(t, items[8].SellIn, -26)
-//assert.Equal(t, items[8].Quality, 0)
+	days := 30
+	for day := 1; day < days; day++ {
+		gildedrose.UpdateQuality(items)
+	}
+
+	assert.Equal(t, items[0].SellIn, -26)
+	assert.Equal(t, items[0].Quality, 0)
+}
